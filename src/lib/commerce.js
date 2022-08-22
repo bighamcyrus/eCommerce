d932201eba4396635f8a9674632cc30b55124bb4
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-22 11:03:34 -0700

    initialize repo

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,4 @@
 import Commerce from '@chec/commerce.js'
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce('{pk_test_462216eb988313a18e843caa941f1872da0f04be2b996}', true);
 
