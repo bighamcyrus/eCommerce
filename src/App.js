020c577fb6c83f16656a3f26840c27feb78455b1
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-22 11:28:09 -0700

    working on commerce

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { commerce } from './lib/commerce';
+import {  commerce } from './lib/commerce';
 
 import { Products, Navbar } from './components'
 
